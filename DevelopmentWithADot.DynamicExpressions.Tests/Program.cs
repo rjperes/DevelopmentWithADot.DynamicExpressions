8869542b73054b255cbe1c9d0c86edd96d8c52e3
Ricardo Peres <rjperes@hotmail.com>
2013-06-18 17:05:36 +0100

    Initial version

---
+++ DevelopmentWithADot.DynamicExpressions.Tests/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace DevelopmentWithADot.DynamicExpressions.Tests
+{
+	class Program
+	{
+		static void Main(String[] args)
+		{
+			var expression = DateTime.Now.ParseExpression<DateTime, Int32>("Day");
+			var day = expression(DateTime.Now);
+			var result = DateTime.Now.EvaluateExpression<DateTime, Int32>("Day");
+			var month = DateTime.Now.EvaluateExpression("Month");
+			var monthString = DateTime.Now.EvaluateExpression("Year.ToString()");
+			var filtered = Enumerable.Range(0, 10).FilterByExpression("true").ToList();
+		}
+	}
+}
---
+++ DevelopmentWithADot.DynamicExpressions/Extensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Web.UI;
+
+namespace DevelopmentWithADot.DynamicExpressions
+{
+	public static class Extensions
+	{
+		public static Func<TType, TResult> ParseExpression<TType, TResult>(this TType source, String expression, params Object [] values)
+		{
+			//public static Expression<Func<T, S>> DynamicExpression.ParseLambda<T, S>(string expression, params object[] values);
+			Assembly asm = typeof(UpdatePanel).Assembly;
+			Type dynamicExpressionType = asm.GetType("System.Web.Query.Dynamic.DynamicExpression");
+			MethodInfo parseLambdaMethod = dynamicExpressionType.GetMethods(BindingFlags.Public | BindingFlags.Static).Where(m => (m.Name == "ParseLambda") && (m.GetParameters().Length == 2)).Single().MakeGenericMethod(typeof(TType), typeof(TResult));
+			Expression<Func<TType, TResult>> accessor = parseLambdaMethod.Invoke(null, new Object[] { expression, values }) as Expression<Func<TType, TResult>>;
+			
+			return (accessor.Compile());
+		}
+
+		public static TResult EvaluateExpression<TType, TResult>(this TType source, String expression, params Object[] values)
+		{
+			return (ParseExpression<TType, TResult>(source, expression, values)(source));
+		}
+
+		public static Object EvaluateExpression<TType>(this TType source, String query, params Object[] values)
+		{
+			return (ParseExpression<TType, Object>(source, query, values)(source));
+		}
+
+		public static IEnumerable<TType> FilterByExpression<TType>(this IEnumerable<TType> source, String expression, params Object[] values)
+		{
+			Func<TType, Boolean> filter = ParseExpression<TType, Boolean>(default(TType), expression, values);
+
+			return (source.Where(filter));
+		}
+	}
+}
